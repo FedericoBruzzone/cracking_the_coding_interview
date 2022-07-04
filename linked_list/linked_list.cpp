c8923a9bd880cb4f928cff48403b33d45a520db3
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-07-04 17:25:15 +0200

    How to write linked_list Node struct

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -1,2 +1,27 @@
 #include <iostream>
 
+/*typedef struct 
+{
+    int value;
+    Node* next;
+} Node; 
+
+
+struct Node
+{
+    int value;
+    Node* next;
+}; */
+
+template <typename T>
+struct Node
+{
+    T value;
+    Node* next;
+};
+typedef Node<int> iNode;
+
+int main()
+{
+    return 0;
+}
