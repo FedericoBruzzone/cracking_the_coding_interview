8e4c116c47b4a04fbf7cfb4eb130f05610fd9caa
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-07-20 01:59:52 +0200

    Add chapter 2

---
+++ cracking_the_coding_interview/chapter_2/2.1_remove_dups.cpp
@@ -0,0 +1,72 @@
+#include "../../ALL_H_FILES.h"
+
+#include "linked_list.cpp"
+
+// First solution 
+// bool removeDups(Node* head) //O(N) time - O(N) space
+// {
+//     if (head == NULL)      return true; 
+//     if (head->next == NULL) return true;
+
+//     std::vector<int> memory (1000);
+//     std::fill(memory.begin(), memory.end(), 0);
+//     Node* node = head;
+
+//     while (node->next != NULL)
+//     {
+//         if (memory[node->data] >= 1)
+//         {
+//             node->deleteNode(head, node->data);
+//         }
+//         else
+//         {
+//             memory[node->data]++;
+//         }
+//         node = node->next;
+//     }
+//     return true;
+// }
+
+// Second solution
+bool removeDups(Node* head) //O(N^2) time - O(1) space
+{
+    if (head == NULL)       return true; 
+    if (head->next == NULL) return true;
+    
+    Node* node = head;
+
+    while (node->next != NULL)
+    {
+        Node* runner = node;
+        while (runner->next != NULL)
+        {
+            if (node->data == runner->next->data)
+            {
+                runner->next = runner->next->next;
+            }
+            else
+            {
+                runner = runner->next;
+            }
+        }
+        node = node->next;
+    }
+    return true; 
+}
+
+int main()
+{
+    Node* n = new Node(10);
+    
+    n->appendToTail(20);
+    
+    n->appendToTail(30); n->appendToTail(30); n->appendToTail(30); 
+
+    n->appendToTail(40); 
+    
+    n->print(n);
+
+    removeDups(n);
+    
+    n->print(n);
+}
---
+++ cracking_the_coding_interview/chapter_2/linked_list.cpp
@@ -0,0 +1,102 @@
+#include "../../ALL_H_FILES.h"
+
+class Node
+{
+public:
+    Node* next = NULL;
+    int data;
+
+    
+    Node(int d)
+    {
+        data = d;
+    }
+    
+    void appendToTail(int d)
+    {
+        Node* end = new Node(d);
+    
+        Node* temp = this;
+    
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = end;
+    }
+
+    Node* deleteNode(Node* head, int d)
+    {
+        if (head == NULL) 
+        { 
+            return NULL; 
+        }
+
+        Node* n = head;
+
+        if (n->data == d)
+        {
+            return head->next;
+        } 
+
+        while (n->next != NULL)
+        {
+            if (n->next->data == d)
+            {
+                n->next = n->next->next;
+                return head;
+            }
+            n = n->next;
+        }
+        return head;
+        
+    }
+
+    void print(Node* head)
+    {
+        if (head == NULL)
+        {
+            LOG("List empty");
+            return;
+        }
+
+        Node* temp;
+
+        for (temp = head; temp != NULL; temp = temp->next)
+            std::cout << temp->data << "-->";
+
+        if(temp == NULL)
+            LOG("NULL");
+    }
+};
+
+// int main()
+// {
+//     Node* n = new Node(10);
+
+//     LOG(n);
+//     LOG(n->data);
+
+//     LOG("Append");
+//     n->appendToTail(20);
+//     LOG(n->next);
+//     LOG(n->next->data);
+//     LOG("");    
+
+//     LOG("Delete");
+//     n->deleteNode(n, 20);
+//     LOG(n);
+//     LOG(n->data);
+//     //LOG(n->next);
+//     //LOG(n->next->data);
+//     LOG("");  
+
+//     LOG("Append");
+//     n->appendToTail(20);
+//     LOG(n);
+//     LOG(n->data);
+//     LOG(n->next);
+//     LOG(n->next->data);
+//     LOG("");
+// }
