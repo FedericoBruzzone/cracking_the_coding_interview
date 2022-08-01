395cbefea01301f5c654dfaad02fefe2d4389311
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-08-01 15:54:58 +0200

    Added 2.6_palindrome

--- cracking_the_coding_interview/chapter_2/2.5_sum_list.cpp
+++ cracking_the_coding_interview/chapter_2/2.5_sum_list.cpp
@@ -1,6 +1,7 @@
 #include "../../ALL_H_FILES.h"
 
 #include "linked_list.cpp"
+//overtype
 
 // First implementation (recursive)
 // Node* addList(Node* n1, Node* n2, int carry)
@@ -39,51 +40,104 @@
 // }
 
 // Second implementation
-// Node* addList(Node* firstNumber, Node* secondNumber)
-// {
-//     int sum = 0;
-//     int carry = 0;
-//     Node* result = firstNumber;
+Node* addList(Node* firstNumber, Node* secondNumber)
+{
+    int sum = 0;
+    int carry = 0;
+    Node* result = firstNumber;
     
-//     while (firstNumber != NULL && secondNumber != NULL)
-//     {
-//         sum = ((firstNumber->data + secondNumber->data) + carry ) % 10;
-//         carry = static_cast<int>((firstNumber->data + secondNumber->data) / 10);
+    while (firstNumber != NULL && secondNumber != NULL)
+    {
+        sum = ((firstNumber->data + secondNumber->data) + carry ) % 10;
+        carry = static_cast<int>((firstNumber->data + secondNumber->data) / 10);
         
-//         firstNumber->data = sum;
+        firstNumber->data = sum;
         
-//         firstNumber = firstNumber->next;
-//         secondNumber = secondNumber->next;
-//     }
+        firstNumber = firstNumber->next;
+        secondNumber = secondNumber->next;
+    }
 
-//     return result;
-// }
+    return result;
+}
 
 // FOLLOW UP
-class PartialSum
-{
-public:
-    Node* sum = NULL;
-    int carry = 0;
-};
+// class PartialSum
+// {
+// public:
+//     Node* sum = NULL;
+//     int carry = 0;
 
-Node* addList(Node* n1, Node* n2)
-{
-    int len1 = n1->length();
-    int len2 = n2->length();
+//     PartialSum() {}
+// };
 
-    if (len1 > len2)
-    {
-        padList(n1, len2 - len1);
-    }
-    else 
-    {
-        padList(n2, len1 - len2);
-    }
+// Node* insertBefore(Node* list, int data)
+// {
+//     Node* node = new Node(data);
 
-    
+//     if (list != NULL)
+//     {
+//         node->next = list;
+//     }
 
-}
+//     return node;
+// }
+
+// Node* padList(Node* node, int padding)
+// {
+//     Node* head = node;
+
+//     for (int i = 0; i < padding; i++)
+//     {
+//         head = insertBefore(head, 0);
+//     }
+//     return head;
+// }
+
+// PartialSum addListHelper(Node* n1, Node* n2)
+// {
+//     if (n1 == NULL && n2 == NULL) 
+//     {
+//         PartialSum sum;
+//         return sum;
+//     } 
+
+//     PartialSum sum = addListHelper(n1->next, n2->next);
+
+//     int val = sum.carry + n1->data + n2->data;
+
+//     Node* full_result = insertBefore(sum.sum, val % 10);
+
+//     sum.sum = full_result;
+//     sum.carry = val / 10;
+//     return sum;
+// }
+
+// Node* addList(Node* n1, Node* n2)
+// {
+//     int len1 = n1->length(n1);
+//     int len2 = n2->length(n2);
+
+//     if (len1 < len2)
+//     {
+//         n1 = padList(n1, len2 - len1);
+//     }
+//     else 
+//     {
+//         n2 = padList(n2, len1 - len2);
+//     }
+
+//     PartialSum sum = addListHelper(n1, n2);
+
+//     if (sum.carry == 0)
+//     {
+//         return sum.sum;
+//     }
+//     else
+//     {
+//         Node* result = insertBefore(sum.sum, sum.carry);
+//         return result;
+//     }
+// }
 
 
 int main()
