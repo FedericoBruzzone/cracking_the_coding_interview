b8c8026ffb942a87dcd05bac22c1008f814fa888
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-08-07 19:01:05 +0200

    Update some file

--- cracking_the_coding_interview/chapter_3/3.4_queue_via_stacks.cpp
+++ cracking_the_coding_interview/chapter_3/3.4_queue_via_stacks.cpp
@@ -2,7 +2,51 @@
 
 #include "stack.cpp"
 
+template <class T>
 class MyQueue
 {
+public:
+    Stack<T>* stackNewest; 
+    Stack<T>* stackOldest;
 
+    MyQueue()
+    {
+        stackNewest = new Stack<T>();
+        stackOldest = new Stack<T>();
+    }
+
+    void shiftStacks()
+    {
+        if (stackOldest->isEmpty())
+        {
+            while (!stackNewest->isEmpty())
+            {
+                stackOldest->push(stackOldest->pop());
+            }
+        }
+    }
+
+    void add(T data)
+    {
+        stackOldest->push(data);
+    }
+
+    T remove()
+    {
+        shiftStacks();
+        return stackOldest->pop();
+    }
+    
+    T peek()
+    {
+        shiftStacks();
+        return stackOldest->peek();
+    }
 };
+
+// =============================================================
+
+int main()
+{
+
+}
