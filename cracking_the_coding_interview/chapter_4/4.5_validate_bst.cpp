80bf7dfab7745d712c2cdc432cf5eb6bab57562f
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-08-31 17:12:33 +0200

    Update 4.5

--- cracking_the_coding_interview/chapter_4/4.5_validate_bst.cpp
+++ cracking_the_coding_interview/chapter_4/4.5_validate_bst.cpp
@@ -5,11 +5,42 @@
 
 // =============================================================
 
+// First implementation
+template <typename T>
+bool isBinarySearchTree(const NodePtr<T> &node, const T* lastVisited)
+{
+    if (node == nullptr) { return true; }
 
+    if (!isBinarySearchTree(node->getLeft(), lastVisited)) { return false; }
+
+    if (lastVisited != nullptr && node->getValue() <= *lastVisited) { return false; }
+
+    lastVisited = &(node->getValue()); 
+
+    if (!isBinarySearchTree(node->getRight(), lastVisited)) { return false; }
+
+    return true;
+}
+
+template <typename T>
+bool isBinarySearchTree(const Tree<T> &tree)
+{
+    return isBinarySearchTree<T>(tree.getRoot(), nullptr);
+}
 
 // =============================================================
 
 int main()
 {
+    auto tree = TestUtils::getSampleTree<int>(20);
+    TestUtils::printTree(tree);
+    std::cout << "The tree is " << (isBinarySearchTree<int>(tree) ? "" : "NOT ") << "binary search tree" << std::endl;
 
+    tree.getRoot()->getRight()->getRight()->getRight()->setLeftChild(std::make_shared<Node<int>>(19));
+    TestUtils::printTree(tree);
+    std::cout << "The tree is " << (isBinarySearchTree<int>(tree) ? "" : "NOT ") << "binary search tree" << std::endl;
+
+    tree.getRoot()->getRight()->getRight()->getRight()->setLeftChild(std::make_shared<Node<int>>(15));
+    TestUtils::printTree(tree);
+    std::cout << "The tree is " << (isBinarySearchTree<int>(tree) ? "" : "NOT ") << "binary search tree" << std::endl;
 }
