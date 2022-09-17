957dc8402f44b406505dc7fe663992eb30e9b144
Federico Bruzzone <federico.bruzzone.i@gmail.com>
2022-09-17 11:55:27 +0200

    Ch.5: Add 5.2 second implementation
    Signed-off-by: Federico Bruzzone <federico.bruzzone.i@gmail.com>

--- cracking_the_coding_interview/chapter_5/5.2_binary_to_string.cpp
+++ cracking_the_coding_interview/chapter_5/5.2_binary_to_string.cpp
@@ -2,47 +2,78 @@
 
 // =============================================================
 
-std::string printBinary(double num)
+// First implementation
+std::string printBinary1(double num)
 {
     if (num > 1 || num < 0) { return "ERROR"; }
     if (num == 1) { return "1"; }
     if (num == 0) { return "0"; }
 
-    std::stringstream s;
-    s << "0.";
+    std::stringstream ss;
+    ss << "0.";
 
     while (num != 0)
     {
-        if (s.tellp() > 32) { return "ERROR"; }
+        if (ss.tellp() > 32) { return "ERROR"; }
 
         double tmp = 2 * num;
         if (tmp >= 1) 
         { 
-            s << '1'; 
+            ss << '1'; 
             num = tmp - 1;
         }
         else
         {
-            s << '0';
+            ss << '0';
             num = tmp;
         }
 
     }
-    return s.str();
+    return ss.str();
 }
+
+// Second implementation
+std::string printBinary2(double num)
+{
+    if (num > 1 || num < 0 ) { return "ERROR"; }
+    if (num == 1) { return "1"; }
+    if (num == 0) { return "0"; }
+
+    std::stringstream ss;
+    ss << "0.";
+    double mantissaBit = 0.5;
+    
+    while (num != 0)
+    {
+        if (ss.tellp() > 32) { return " ERROR"; }
+
+        if (num >= mantissaBit) 
+        {
+            ss << '1';
+            num -= mantissaBit;
+        }
+        else
+        {
+            ss << '0';
+        }
+
+        mantissaBit /= 2;
+    }
+    return ss.str();
+}  
 
 // =============================================================
-int main()
-{
-    double value = 0;
-    double frac = 1.0 / 2 + 1.0 / 8;
+// int main()
+// {
+//     double value = 0;
+//     double frac = 1.0 / 2 + 1.0 / 8;
 
-    std::cout << std::setprecision(std::numeric_limits<double>::digits10 + 1);
+//     std::cout << std::setprecision(std::numeric_limits<double>::digits10 + 1);
 
-    for (int i = 0; i < 10; ++i)
-    {
-        std::cout << value << ": " << printBinary(value) << "(b), " << printBinary(value) << "(b)\n";
-        value += frac;
-        frac /= 16;
-    }
-}
+//     for (int i = 0; i < 10; ++i)
+//     {
+//         std::cout << value << ": " << printBinary1(value) << "(b), " << printBinary2(value) << "(b)\n";
+//         value += frac;
+//         frac /= 16;
+//     }
+// }
